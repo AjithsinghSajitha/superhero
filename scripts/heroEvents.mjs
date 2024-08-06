1d15090085ce6dd96341d4d84c0df609bc5b4464
Ajithsingh <ajithsingh.a.s+github@gmail.com>
2024-08-06 17:31:19 +0530

    spelling mistake

--- scripts/heroEvents.mjs
+++ scripts/heroEvents.mjs
@@ -1,7 +1,7 @@
 
 export const heroPage = (element, data) => {
     element.addEventListener('click', ()=>{
-        window.Open('/pages/hero.html');
+        window.open('/pages/hero.html');
         localStorage.setItem("Hero",JSON.stringify(data));
     });
     
