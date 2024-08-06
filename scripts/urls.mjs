58689e2d84d306133ff72db6ecd300e3f23c7933
Ajithsingh <ajithsingh.a.s+github@gmail.com>
2024-08-06 17:15:32 +0530

    added fix for redirect again and urls are now https

--- scripts/urls.mjs
+++ scripts/urls.mjs
@@ -3,4 +3,4 @@ export const heroUrl = (hero) =>
 export const url = () =>
     "https://gateway.marvel.com/v1/public/characters?apikey=36394e08b19938075eb6d89ad0792c70&ts=1721807370&hash=33cd5c84466598587c29794cee7adbe0&offset=0&limit=5";
 export const comicUrl = (hero) => 
-    `http://gateway.marvel.com/v1/public/characters/${hero}/comics?apikey=36394e08b19938075eb6d89ad0792c70&ts=1721807370&hash=33cd5c84466598587c29794cee7adbe0&offset=0&limit=5`;
+    `https://gateway.marvel.com/v1/public/characters/${hero}/comics?apikey=36394e08b19938075eb6d89ad0792c70&ts=1721807370&hash=33cd5c84466598587c29794cee7adbe0&offset=0&limit=5`;
