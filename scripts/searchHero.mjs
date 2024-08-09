040045f4f4dc8c481b45fd03ebd3516b73acbe29
Ajithsingh <ajithsingh.a.s+github@gmail.com>
2024-08-10 01:08:00 +0530

    unable to continue development because of RequestThrottled

--- scripts/searchHero.mjs
+++ scripts/searchHero.mjs
@@ -18,6 +18,7 @@ const searchResult = async () => {
       data.data.results.map((hero, i) => {
         let li = document.createElement("li");
         let span = document.createElement("span");
+        let favorite = document.createElement("span");
         let img = document.createElement("img");
         let name = hero.name;
         let thumb = hero.thumbnail;
@@ -29,9 +30,41 @@ const searchResult = async () => {
         img.setAttribute("src", path);
         img.setAttribute("alt", name);
         span.innerText = `${i + 1}) ${name}`;
+        favorite.innerHTML ='<i class="fa-solid fa-star"></i>';
 
         li.append(img);
+        li.append(favorite);
         li.append(span);
+
+        favorite.addEventListener('click',(e)=> {
+          e.stopPropagation();
+          if(!localStorage.getItem('favorite')){
+            localStorage.setItem('favorite', JSON.stringify([hero]))
+          }
+          else{
+            let fav = JSON.parse(localStorage.getItem("favorite"));
+            // let item = fav.filter(item => item.id == hero.id)
+            let index = -1;
+            let isAlreadyExist;
+            fav.every((item, i) => {
+              if(item.id == hero.id){
+                isAlreadyExist = true;
+                index = i;
+                return false;
+              }
+              return true;
+            });
+            
+            if(isAlreadyExist){
+              console.log(index);
+            }
+            else{
+              fav.push(hero);
+              localStorage.setItem('favorite', JSON.stringify(fav));
+            }
+            
+          }
+        })
 
         li.addEventListener('click', () => {
             localStorage.setItem("Hero",JSON.stringify(hero));
