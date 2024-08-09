a44c9f35110a9520628b7080b825913e3858003b
Ajithsingh Athmasaran Sajitha <s.ajithsingh@noxum.com>
2024-08-09 12:47:29 +0530

    Search result design completed need loading spinner

--- scripts/searchHero.mjs
+++ scripts/searchHero.mjs
@@ -2,6 +2,7 @@ import { searchUrl } from "./urls.mjs";
 
 let searchTerm = localStorage.getItem("SearchTerm");
 let searchCount = document.getElementById("search-count");
+let searchResultElement = document.getElementById("search-result");
 
 const searchResult = async () => {
   try {
@@ -9,12 +10,30 @@ const searchResult = async () => {
     const data = await heroData.json();
     let count = parseInt(data.data.total);
     count
-      ? (searchCount.innerText = count)
+      ? (searchCount.innerText = `Result Found: ${count}`)
       : (searchCount.innerText = "no result");
 
     if (count) {
+      let ul = document.createElement("ul");
       data.data.results.map((hero, i) => {
-        console.log(`${i+1}) ${hero.name}`);
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+        let img = document.createElement("img");
+        let name = hero.name;
+        let thumb = hero.thumbnail;
+
+        let path = !thumb
+          ? "https://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+          : `${thumb.path}.${thumb.extension}`;
+
+        img.setAttribute("src", path);
+        img.setAttribute("alt", name);
+        span.innerText = `${i + 1}) ${name}`;
+
+        li.append(img);
+        li.append(span);
+        ul.append(li);
+        searchResultElement.append(ul);
       });
     }
   } catch (error) {}
